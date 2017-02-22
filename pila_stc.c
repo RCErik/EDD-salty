db4e4f5f749133b849c04ccdd0ae90fb60289f59
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-02-21 19:00:40 -0600

    Update pila_stc.c

--- pila_stc.c
+++ pila_stc.c
@@ -1,1 +1,38 @@
+#include "pila_stc.h"
+
+int Size(pila *s){
+    return s -> tope + 1;
+}
+
+boolean Empty(pila *s){
+    boolean r;
+    if(s -> tope >= 0)
+        r = FALSE;
+    else
+        r = TRUE;
+    return r;
+}
+
+void Initialize(pila *s){
+    s -> tope = -1;
+    return;
+}
+
+void Destroy(pila *s){
+    Initialize(s);
+    return;
+}
+
+void Push(elemento e, pila *s){
+    s -> tope++;
+    s -> A[s -> tope] = e;
+    return;
+}
+
+elemento Pop(pila *s){
+    elemento r;
+    r = s -> A[s -> tope];
+    s -> tope--;
+    return r;
+}
 
