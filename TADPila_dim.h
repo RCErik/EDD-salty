5a6a07d2c28ea4534c27fd946c44bee080fc1df6
Erik <ecerikrodriguez@outlook.com>
2017-02-21 19:30:33 -0600

    .h de TADPila_dim.c
    Elementos clave de la pila dinámica.

---
+++ TADPila_dim.h
@@ -0,0 +1,32 @@
+#define TRUE 1
+#define FALSE 0
+#include <stdlib.h>
+
+typedef char boolean;
+
+typedef struct elemento
+{
+  int num; //numero
+  char let; //letra
+  float dec; //decimal
+}elemento;
+
+typedef struct nodo
+{
+  elemento e;
+  struct nodo *abajo;
+}nodo;
+
+typedef struct pila
+{
+  nodo *tope;
+}pila;
+
+void initialize (pila *s);  //Inicializa una pila
+void Push (elemento e, pila *s); //Mete un elemento a la pila
+elemento Pop (pila *s); //Saca el elemento tope de la pila
+boolean Empty (pila *s); //Verifica el estado de la pila
+int Size (pila *s); //Muestra el tamaño de la pila
+elemento Top (pila *s); //Muestra el elemento tope de la pila
+void Destroy (pila *s); //Destruye la pila
+
