5a3931d6ee724c4f14d9fc69129bc78047a802da
Erik <ecerikrodriguez@outlook.com>
2017-02-21 19:21:16 -0600

    TADPila dinamica
    El código para la pila dinámica :)

--- TADPila_dim.c
+++ TADPila_dim.c
@@ -1,36 +1,72 @@
 #include"TADPila_dim.h"
-void initialize (pila *s)
+
+void 
+initialize (pila *s)
 {
-s->tope=NULL;
-return;
+  s->tope=NULL;
+  return;
 }
 
-void Push (elemento e, pila *s)
+void 
+Push (elemento e, pila *s)
 {
-nodo *aux;
-aux = malloc(sizeof(nodo));
-aux->abajo = s->tope;
-s->tope = aux;
-aux->e = e;
-return;
+  nodo *aux;
+  aux = malloc(sizeof(nodo));
+  aux->abajo = s->tope;
+  s->tope = aux;
+  aux->e = e;
+  return;
 }
 
-elemento Pop (pila *s)
+elemento 
+Pop (pila *s)
 {
-elemento r;
-nodo *aux;
-r = s->tope->e;
-aux = s->tope;
-s->tope = s->tope->abajo;
-free(aux);
-return r;
+  elemento r;
+  nodo *aux;
+  r = s->tope->e;
+  aux = s->tope;
+  s->tope = s->tope->abajo;
+  free(aux);
+  return r;
 }
 
-boolean Empty (pila *s)
+boolean 
+Empty (pila *s)
 {
-if (s->tope == NULL)
-  return TRUE;
-return FALSE;
+  if (s->tope == NULL)
+    return TRUE;
+  return FALSE;
 }
 
-int Size
+int 
+Size (pila *s)
+{
+  nodo *aux;
+  int tamaño = 0;
+  aux = s->tope;
+  while (aux != NULL)
+  {
+  aux = aux->abajo;
+  tamaño ++;
+  }
+  return tamaño;
+}
+
+elemento 
+Top (pila *s)
+{
+  return s->tope->e;
+}
+
+void 
+Destroy (pila *s)
+{
+  nodo *aux;
+  while (s->tope != NULL)
+  {
+    aux = s->tope->abajo;
+    free(s->tope);
+    s->tope = aux;
+  }
+  return;
+}
