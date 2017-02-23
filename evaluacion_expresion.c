4959696f45c9141f353a2d2b0da6aa4eddda2938
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-02-23 13:20:04 -0600

    Borrador malo :p

--- evaluacion_expresion.c
+++ evaluacion_expresion.c
@@ -1,2 +1,25 @@
+//Funcion que evalúa la expresión final en posfijo
+float evaluar(pila *s){
+  float valor;
+  float valor_letra[27];
+  //Inicializamos el arreglo de valores en -1
+  for(int i = 0; i < 27; i++)
+    valor_letra[i] = -1;
+  printf("Indique el valor de la letra:\n");
+  scanf("%f",&valor); 
+  //Solicitamos los valores de las letras encontradas en la expresion
+  verificar_valor_disponible(valor, convertir_ASCII(caracter), valor_letra);
+};
 
 
+int convertir_ASCII(char letra){
+  int valor_ASCII;
+  valor_ASCII = (int)letra - 64;
+  return valor_ASCII;
+}
+
+void verificar_valor_disponible(float numero, int posicion, float valores[27]){
+  if(valores[posicion] != -1)
+    valores[posicion] = numero;
+  return;
+}
