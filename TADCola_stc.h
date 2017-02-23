2d29fe4613e24ae1d19caa1b45fa00ba46620c56
Erik <ecerikrodriguez@outlook.com>
2017-02-22 19:45:22 -0600

    el .h de TADCola_stc.c

--- TADCola_stc.h
+++ TADCola_stc.h
@@ -1,1 +1,30 @@
+#define TRUE 1
+#define FALSE 0
+#define MAX_ELEMENT 1000
+
+typedef char boolean;
+
+typedef struct elemento
+{
+  int num;  //numero
+  char let;  //letra
+  float dec;  //decimal
+}
+
+typedef struct cola
+{
+  int frente;
+  int final;
+  elemento A[MAX_ELEMENT];
+}
+
+void Initialize (cola *c);
+void Queue (elemento e, cola *c);
+elemento Dequeue (cola *c);
+boolean Empty (cola *c);
+int Size (cola *c);
+elemento Front (cola *c);
+elemento Final (cola *c);
+elemento Element (int n, cola *c);
+void Destroy (cola *c);
 
