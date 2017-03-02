bd7e8e6225c4cef6029ca848efff00c6859a6335
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-03-02 13:06:06 -0600

    Update main_practica1.c

--- main_practica1.c
+++ main_practica1.c
@@ -1,9 +1,6 @@
-/*
-
 #include "TADPila_stc.h"
 #include "expresion_posfija.h"
 #include "evaluacion.h"
-#include <windows.h>
 
 #define TAMANIO 100
 
@@ -15,12 +12,18 @@ int main(void){
 	printf("\n\t\tEscribe la expresión que deseas evaluar :3");
 	scanf("&s", &expresion);
 	tam_cadena = strlen(expresion);
+	/* 
+	    Ciclo que se ejecuta en caso de que exista un error en 
+	    la evaluación de los paréntesis, para que el usuario
+	    introduzca de nueva cuenta la cadena.
+	*/
 	while(validar_expresion(expresion, tam_cadena) == FALSE){
 		printf("\n\t\tEscribe la expresión que deseas evaluar :3");
 		scanf("&s", &expresion);
 		tam_cadena = strlen(expresion);
 		validar_expresion(expresion, tam_cadena);	
 	}
+	//A partir de aquí se ejecutan las demás funciones de forma normal
 	printf("\n\n\t\tEstamos convirtiendo tu expresión a posfijo, espera porfi n.n\n\n\t\t");
 	expresion_posfija = Expresion_Posfija(expresion, expresion_posfija, tam_cadena);
 	printf("\n\n\t\tTu expresión en forma posfija es la siguiente: \n\n\t\t %s", expresion_posfija);
