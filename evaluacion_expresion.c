e03036ce1209121be030363b6c0b22cc299105c6
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-03-02 22:01:57 -0600

    Update evaluacion_expresion.c

--- evaluacion_expresion.c
+++ evaluacion_expresion.c
@@ -1,25 +1,71 @@
-//Funcion que evalúa la expresión final en posfijo
-float evaluar(pila *s){
-  float valor;
-  float valor_letra[27];
-  //Inicializamos el arreglo de valores en -1
-  for(int i = 0; i < 27; i++)
-    valor_letra[i] = -1;
-  printf("Indique el valor de la letra:\n");
-  scanf("%f",&valor); 
-  //Solicitamos los valores de las letras encontradas en la expresion
-  verificar_valor_disponible(valor, convertir_ASCII(caracter), valor_letra);
-};
+/*
+	AUTOR: Carlos Armando Ochoa Ginera
+	VERSIÓN: 1.2
+	
+	DESCRIPCIÓN: Función que nos permite obtener los valores que el usuario
+	desea dar a la expresión postfija y nos permite calcular el resultado de dicha expresión
+	
+	OBSERVACIONES: En esta versión se precisa de una pila de elementos de tipo entero, 
+	sin embargo esto puede cambiar según Erik me la haga de a tos xD
+*/
 
+#include <stdio.h>
+#include <math.h>
+#include "TADPila_stc.h"
 
-int convertir_ASCII(char letra){
-  int valor_ASCII;
-  valor_ASCII = (int)letra - 64;
-  return valor_ASCII;
+#define TAMABECEDARIO 27
+#define ASCIIUNO 64
+#define ASCIIFIN 91
+
+void evaluar_expresion(char expresion[100], int tam_cadena){
+	Pila pila_evaluacion;
+	int i, caracter, valor, operando_uno, operando_dos;
+	int abecedario[TAMABECEDARIO] = {-1};
+	Initialize(&pila_evaluacion);
+	for(i = 0; i < tam_cadena ; i++){
+		caracter = (int) expresion[i];
+		if(caracter > ASCIIUNO && caracter < ASCIIFIN){
+			if(abecedario[caracter - ASCIIUNO] != -1){
+				printf("Indique el valor que le quiere dar a %c por favor :3", expresion[i]);
+				scanf("%d", &valor);
+				abecedario[caracter - ASCIIUNO] = valor;	
+			}	
+		}
+	}
+	for(i = 0; i < tam_cadena ; i++){
+		caracter = (int) expresion[i];
+		if(caracter > ASCIIUNO && caracter < ASCIIFIN)
+			valor = abecedario[caracter - ASCIIUNO];
+			Push(&pila_evaluacion, valor);
+		} else if (expresion[i] == '+'){
+			operando_uno = Pop(&pila_evaluacion);
+			operando_dos = Pop(&pila_evaluacion);
+			valor = operando_uno + operando_dos;
+			Push(&pila_evaluacion, valor);
+		} else if (expresion[i] == '-'){
+			operando_uno = Pop(&pila_evaluacion);
+			operando_dos = Pop(&pila_evaluacion);
+			valor = operando_uno - operando_dos;
+			Push(&pila_evaluacion, valor);
+		} else if (expresion[i] == '*'){
+			operando_uno = Pop(&pila_evaluacion);
+			operando_dos = Pop(&pila_evaluacion);
+			valor = operando_uno * operando_dos;
+			Push(&pila_evaluacion, valor);
+		} else if (expresion[i] == '/'){
+			operando_uno = Pop(&pila_evaluacion);
+			operando_dos = Pop(&pila_evaluacion);
+			valor = operando_uno / operando_dos;
+			Push(&pila_evaluacion, valor);
+		} else if (expresion[i] == '^'){
+			operando_uno = Pop(&pila_evaluacion);
+			operando_dos = Pop(&pila_evaluacion);
+			valor = pow(operando_uno, operando_dos);
+			Push(&pila_evaluacion, valor);
+		}
+	}
+	valor = Pop(&pila_evaluacion);
+	printf("El resultado de la expresión es: %d", valor);
+	return;	
 }
 
-void verificar_valor_disponible(float numero, int posicion, float valores[27]){
-  if(valores[posicion] != -1)
-    valores[posicion] = numero;
-  return;
-}
