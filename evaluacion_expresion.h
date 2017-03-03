f899095783ffcc1a5954273582faa52afd06189c
Erik <ecerikrodriguez@outlook.com>
2017-03-03 13:44:53 -0600

    .h de evaluacion_expresion.c

---
+++ evaluacion_expresion.h
@@ -0,0 +1,19 @@
+/*
+	AUTOR: Carlos Armando Ochoa Ginera
+	VERSIÓN: 1.2
+	
+	DESCRIPCIÓN: Función que nos permite obtener los valores que el usuario
+	desea dar a la expresión postfija y nos permite calcular el resultado de dicha expresión
+	
+	OBSERVACIONES: En esta versión se precisa de una pila de elementos de tipo entero, 
+	sin embargo esto puede cambiar según Erik me la haga de a tos xD
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+#define TAMABECEDARIO 27
+#define ASCIIUNO 64
+#define ASCIIFIN 91
+
+void evaluar_expresion(char expresion[100], int tam_cadena);
