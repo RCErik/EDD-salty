7058696543c59f5202adfd18bcd265c8e9b8b8b7
Erik <ecerikrodriguez@outlook.com>
2017-03-03 13:38:00 -0600

    Ver 1.3
    Se arreglaron condiciones que impedian la transformacion de la expresion infija a posfija y errores menores.

--- expresion_posfija.c
+++ expresion_posfija.c
@@ -12,9 +12,7 @@ OBSERVACIONES: Se emplea la libreria TADPilaDin.h o TADPilaEst.h
 
 
 #include "TADPilaDin.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "expresion_posfija.h"
 
 char *
 Expresion_Posfija (char cadena[150], char cadena_aux[150], int tam_cadena)
