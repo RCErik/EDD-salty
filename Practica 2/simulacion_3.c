db7c32d2d2070e687f6f59ea41dfcc5c05f5d533
Erik <ecerikrodriguez@outlook.com>
2017-04-10 15:52:40 -0500

    Update simulacion_3.c

--- Practica 2/simulacion_3.c
+++ Practica 2/simulacion_3.c
@@ -89,18 +89,24 @@ main (void)
   int tiempos[4];		//Tiempo de cada caja, 0 el tiempo de atencion de las cajas, 1-3 tiempo que llega la gente.
   int personas[3];		//Las personas de las colas, 0 cliente, 1 usuario, 2 preferente.
   elemento aux;			//Elemento auxiliar para meter elementos a la cola.
-  elemento cajero[10];		//arreglo que sirve para la funcion de imprimir banco
+  /*OPCIONES:
+  Usar elemento cajero[10]; que solo es una linea que aparenta ser todas las cajas de atencion
+  O TAMBIEN
+  Usar cola cajero[10] y solo usar el front de las colas para ver quien esta siendo atendido en ese momento, 
+  y seria mas facil de manejar y de ver, por lo cual yo recomiendo esta pero ya me dices cual quieres tu charly.*/
   cola filas[3];		//Colas que son las filas de los usuarios, clientes y preferentes.
 
   srand (time (NULL));		//a lo mejor se quita esta linea
 
-  for (cont = 0; cont < 3; cont++)	//Ciclo que inicializa las colas y arreglos.
+  for (cont = 0; cont < 3; cont++)	//Ciclo que inicializa las colas de personas y arreglos de tiempo.
     {
       Initialize (&fila[cont]);
       personas[cont] = 0;
       tiempos[cont] = 0;
     }
   tiempos[3] = 0;
+ /* for (cont = 0; cont < 10; cont++)	//Ciclo que inicializa los cajeros, en caso de que se prefiera la segunda ocpion
+      Initialize (&cajero[cont]);  */
 
   //Instrucciones para obtener la informacion del usuario.
   printf ("Cuantas cajeros atienden:   ");
@@ -146,7 +152,7 @@ main (void)
 	  Queue (&fila[2], aux);	//Mete al cliente 'n' a la cola.
       }
      
-      if() //Modulo del tiempo con el que son atendidos es 0 hace push de las filas para estar en el cajero.
+      if((tiempomod % tiempos[0]) == 0) //Modulo del tiempo con el que son atendidos es 0 hace push de los cajeros.
       {
 	  k     
       }
